af32bc16d358c855e9978f14bd2bf5900b70c8df
Gavin Andresen <gavinandresen@gmail.com>
2013-03-23 18:14:12 -0400

    Have Qt poll for shutdown requested, the QT way.
    Conflicts:
    	src/qt/bitcoin.cpp
    	src/qt/bitcoingui.cpp
    	src/qt/bitcoingui.h

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -189,6 +189,9 @@ private slots:
 
     void updateWeight();
     void updateStakingIcon();
+
+    /** called by a timer to check if fRequestShutdown has been set **/
+    void detectShutdown();
 };
 
 #endif
