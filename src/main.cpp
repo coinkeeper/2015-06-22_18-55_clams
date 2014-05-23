b0002446d9843262269342874264802a9e3a0659
xploited <xploited@unknown000C29DB7CCD.gateway.2wire.net>
2014-05-23 02:36:37 -0700

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -2824,7 +2824,7 @@ bool static AlreadyHave(CTxDB& txdb, const CInv& inv)
 // The message start string is designed to be unlikely to occur in normal data.
 // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
 // a large 4-byte int at any alignment.
-unsigned char pchMessageStart[4] = { 0x70, 0x35, 0x22, 0x05 };
+unsigned char pchMessageStart[4] = { 0x03, 0x22, 0x35, 0x15 };
 
 bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
 {
