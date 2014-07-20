83a4fa781d358e176563346de810e379f23e037b
Administrator <admin@local.host>
2014-07-19 19:54:12 -0700

    fix a first-time start crash bug

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -74,6 +74,7 @@ bool OptionsModel::Upgrade()
     settings.setValue("bImportFinished", true);
 
     // Move settings from old wallet.dat (if any):
+    assert(!strWalletFileName.empty())
     CWalletDB walletdb(strWalletFileName);
 
     QList<QString> intOptions;
