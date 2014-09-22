1c0f2ceda81a9ce0cc82ff5f1d6537d90a2f1a14
Pavel Vasin <pavel@vasin.nl>
2014-09-22 22:17:56 +0400

    remove unused ToStringShort() methods

--- src/main.h
+++ src/main.h
@@ -262,11 +262,6 @@ public:
         return !(a == b);
     }
 
-    std::string ToStringShort() const
-    {
-        return strprintf(" %s %d", prevout.hash.ToString().c_str(), prevout.n);
-    }
-
     std::string ToString() const
     {
         std::string str;
@@ -353,11 +348,6 @@ public:
     friend bool operator!=(const CTxOut& a, const CTxOut& b)
     {
         return !(a == b);
-    }
-
-    std::string ToStringShort() const
-    {
-        return strprintf(" out %s %s", FormatMoney(nValue).c_str(), scriptPubKey.ToString(true).c_str());
     }
 
     std::string ToString() const
@@ -513,13 +503,6 @@ public:
     friend bool operator!=(const CTransaction& a, const CTransaction& b)
     {
         return !(a == b);
-    }
-
-    std::string ToStringShort() const
-    {
-        std::string str;
-        str += strprintf("%s %s", GetHash().ToString().c_str(), IsCoinBase()? "base" : (IsCoinStake()? "stake" : "user"));
-        return str;
     }
 
     std::string ToString() const
