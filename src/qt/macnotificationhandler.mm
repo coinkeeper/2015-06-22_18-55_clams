0a299a157f9c8d589cf92da6cdd7c104835f3417
l0rdicon <xploited.ca@gmail.com>
2014-12-30 15:46:20 -0400

    Updating notifiy to clamfoundation.Clam-Qt

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.bitcoinfoundation.Bitcoin-Qt";
+        return @"org.clamfoundation.Clam-Qt";
     } else {
         return [self __bundleIdentifier];
     }
